fac6a9064a21dd9ba77b94642e5cb6bb30fef819
tactnician <tactnician@gmail.com>
2021-02-11 10:26:43 -0500

    testing FirstContract.sol ready to deploy on Remix

---
+++ FirstContract.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.25
+
+contract MyContract {
+    string value;
+
+    constructor () public {
+        value = "myValue";
+    }
+
+    function get() public view returns(string) {
+        return value; 
+    }
+
+    function set(string _value ) public {
+        value = _value;
+    }
+
+
+
+}
